dce8d85b689a0ce58c001489b02296cf393230aa
Francisco Martinez <franscisco.martinez@e32.tech>
2022-04-04 13:07:27 -0500

    Se Finalizo el video 119 del curso de vue
    noPageFound

--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import aboutPage from '../modules/pokemon/pages/aboutPage'
 import listPage from '../modules/pokemon/pages/listPage'
 import pokemonPage from '../modules/pokemon/pages/pokemonPage'
-
+import NoPageFound from '../modules/shared/pages/NoPageFound'
 const routes = [
     { path: '/', component: listPage },
     { path: '/about', component: aboutPage },
     { path: '/id', component: pokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
   ]
 
 const router = createRouter({
