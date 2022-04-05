3881800c43c780c6bdb725fc25cd26eb39654780
Francisco Martinez <franscisco.martinez@e32.tech>
2022-04-05 14:35:19 -0500

    Se finalizo el video 124 del curso de vue

--- src/modules/pokemon/pages/aboutPage.vue
+++ src/modules/pokemon/pages/aboutPage.vue
@@ -2,4 +2,65 @@
     <h1>About 
         template
     </h1>
+    <hr>
+    <h2>{{name}}</h2>
+    <button @click="onChangen">
+        cambiar nombre
+    </button>
 </template>
+<script>
+export default{
+    data(){
+        return {
+            
+            name: 'Paquito'
+        }
+    },
+    methods: {
+        onChangen(){
+            console.log('se cambiara el nombre a fransua')
+            this.name = 'Fransua'
+        }
+    },
+    beforeCreate () {
+        console.log('beforeCreate')
+    },
+    created () {
+        console.log('created')
+        //Peticiones HTTP
+    },
+    beforeMount () {
+        console.log('beforeMount')
+    },
+    mounted () {
+        console.log('mounted')
+    },
+    beforeUpdate () {
+        console.log('beforeUpdate')
+    },
+    updated () {
+        console.log('updated')
+    },
+    activated () {  
+        console.log('activated')
+    },
+    deactivated () {
+        console.log('deactivated')
+    },
+    beforeUnmount () {
+        console.log('beforeUnmount')
+    },
+    unmounted () {
+        console.log('unmounted')
+    },  
+    errorCaptured () {
+        console.log('errorCaptured')
+    },
+    renderTracked () {
+        console.log('renderTracked')
+    },
+    renderTriggered () {
+        console.log('renderTriggered')
+    },
+}
+</script>
