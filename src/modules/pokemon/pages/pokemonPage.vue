9bd2d3380cd9ceea50ea2059d8ddc6fbec2349b4
Francisco Martinez <franscisco.martinez@e32.tech>
2022-04-05 15:36:56 -0500

    Se finalizo el video 127 del curso de vue

--- src/modules/pokemon/pages/pokemonPage.vue
+++ src/modules/pokemon/pages/pokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>Pokemon Page <span>#{{id}}</span></h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
 </template>
 <script>
 
@@ -14,6 +17,7 @@ export default {
         return{
         // id: null
         //id: this.$route.params.id
+        pokemon: null
         }
     },
     created(){
@@ -21,7 +25,25 @@ export default {
         //const {id} = this.$route.params
         // console.log(id)
         //this.id = id
-        console.log(this.$attrs)
+        //console.log(this.$attrs)
+        this.getPokemon()
+    },
+    methods: {
+        async getPokemon() {
+            try {
+                const pokemon = await fetch( `https:pokeapi.co/api/v2/pokemon/${this.id}`).then(r => r.json());
+                console.log(pokemon)
+                this.pokemon = pokemon   
+            } catch (error) {
+                this.$router.push('/')
+                console.log('no se encuentra la información requerida')
+            }
+        }
+    },
+    watch: {
+        id(){
+            this.getPokemon()
+        }
     }
 }
 </script>
