20f3d7f90ef4b68b53322fa528c37d25f4aea642
Francisco Martinez <franscisco.martinez@e32.tech>
2022-04-05 16:17:43 -0500

    Se finalizo el video 129 del curso de vue

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,38 @@
+<template>
+    <a v-if="isExternalLink" 
+    target="_blank"
+    class= 'normL-link'
+    :href="link.to">{{link.name}}</a>
+
+    <router-link v-else
+        :to="link.to"
+        v-slot="{href,isActive}">
+
+        <a :href="href"
+           :class="isActive ? 'is-active' : 'normL-link'"
+        >{{link.name}}</a>
+    </router-link>    
+</template>
+<script>
+export default {
+    props: {
+        link: {
+            type: Object,
+            required: true
+        }
+    },
+    computed:{
+        isExternalLink(){
+            return this.link.to.startsWith('http')
+        }
+    }
+}
+</script>
+<style scoped>
+    .isActive{
+        color: #42b983;
+    }
+    .normal-link{
+        color: #c6c5c6
+    }
+</style>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
-        <router-link :to="{name:'home'}">Pokemon List</router-link>
+        <!--<router-link :to="{name:'home'}">Pokemon List</router-link>
         <router-link :to="{name: 'pokemon-id', params:{id: '85'}}">Pokemon Id</router-link>
-        <router-link :to="{name: 'about'}">About</router-link>
+        <router-link :to="{name: 'about'}">About</router-link>-->
+        <CustomLink 
+        v-for="link in link"
+        :key="link.to"
+        :link="link"/>
     </div>
 </template>
+<script>
+import { defineAsyncComponent } from '@vue/runtime-core'
+export default {
+    data() {
+        return {
+            links:[
+                {to: '/home', name:'Pokemons'},
+                {to: '/pokemonid/50', name: 'id'},
+                {to: '/about', name: 'about'},
+                {to: 'https://google.com', name: 'Goolge'},
+            ]
+        }
+    },
+    components: {
+
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }
+}
+</script>
 <style scoped>
      div{
          padding: 0 30px;
@@ -14,7 +37,7 @@
          color: lightblue;
          margin: 0 10px;
      }
-     div .router-link-exact-active{
+    /* div .router-link-exact-active{
          color: #42b983;
-     }
+     }*/
 </style>
